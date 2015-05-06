425169aadb4b5a7d61e814460fb8a35642a349e5
Daniel Picado <dprm23@gmail.com>
2015-05-06 09:47:35 -0600

    Documentación reservación

--- Servicios Reservados 2/Servicios Reservados 2/App_Data/Reservaciones/EntidadReservaciones.cs
+++ Servicios Reservados 2/Servicios Reservados 2/App_Data/Reservaciones/EntidadReservaciones.cs
@@ -14,7 +14,11 @@ namespace Servicios_Reservados_2
         private String solicitante;
         private DateTime fechaInicio;
         private DateTime fechaSalida;
-
+        /*
+         * Requiere: Un identificador de reservacion, una hilera con la anfritriona, una hilera con la estación, una hilera con el número, una hilera con el solicitante, una fecha con el inicio y una fecha con la salida
+         * Efectúa : Inicializa las variables globales de la clase con los parámetros.
+         * Retorna : N/A.
+         */
         public EntidadReservaciones(String id, String anfitriona, String estacion, String numero, String solicitante, DateTime fechaInicio, DateTime fechaSalida)
         {
             Id = id;
@@ -26,32 +30,66 @@ namespace Servicios_Reservados_2
             FechaSalida = fechaSalida;
                 
         } 
-
+        /*
+         * Requiere: una Hilera con el valor nuevo
+         * Efectúa : Asigna a la variable global id con el parámetro
+         * Retorna : El valor de la variable global id
+         */
         public String Id {
             get { return id; } 
             set { id = value;}
         }
+        /*
+         * Requiere: una Hilera con el valor nuevo
+         * Efectúa : Asigna a la variable global Anftritriona con el parámetro
+         * Retorna : El valor de la variable global Anfitriona
+         */
         public String Anfitriona {
             get { return anfitriona; }
             set { anfitriona = value; }
         }
+        /*
+         * Requiere: una Hilera con el valor nuevo
+         * Efectúa : Asigna a la variable global Estación con el parámetro
+         * Retorna : El valor de la variable global Estación
+         */
         public String Estacion {
             get { return estacion; } 
             set { estacion = value;}
         }
+        /*
+         * Requiere: una Hilera con el valor nuevo
+         * Efectúa : Asigna a la variable global Numero con el parámetro
+         * Retorna : El valor de la variable global Numero
+         */
         public String Numero
         {
             get { return numero; }
             set { numero = value; }
         }
+        /*
+         * Requiere: una Fecha con el valor nuevo
+         * Efectúa : Asigna a la variable global FechaInicio con el parámetro
+         * Retorna : El valor de la variable global FechaInicio
+         */
         public DateTime FechaInicio {
             get { return fechaInicio; } 
             set { fechaInicio = value;}
         }
+        /*
+         * Requiere: una Fecha con el valor nuevo
+         * Efectúa : Asigna a la variable global FechaSalida con el parámetro
+         * Retorna : El valor de la variable global FechaSalida
+         */
         public DateTime FechaSalida {
             get { return fechaSalida; } 
             set { fechaSalida = value;}
         }
+        /*
+         * Requiere: una Hilera con el valor nuevo
+         * Efectúa : Asigna a la variable global Solicitante con el parámetro
+         * Retorna : El valor de la variable global Solicitante
+         */
         public String Solicitante {
             get { return solicitante; } 
             set { solicitante = value;}
