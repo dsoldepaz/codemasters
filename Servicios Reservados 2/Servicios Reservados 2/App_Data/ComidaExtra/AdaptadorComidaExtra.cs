d32b3890ab564a079a79ea06040a02bb8ed90432
dsoldepaz <d.soldepaz@gmail.com>
2015-05-12 12:02:14 -0600

    comida extra bd oet

--- Servicios Reservados 2/Servicios Reservados 2/App_Data/ComidaExtra/AdaptadorComidaExtra.cs
+++ Servicios Reservados 2/Servicios Reservados 2/App_Data/ComidaExtra/AdaptadorComidaExtra.cs
@@ -34,7 +34,7 @@ namespace Servicios_Reservados_2
         DataTable dt;
         public AdaptadorComidaExtra()
         {
-             adaptador = new OleDbConnection("Provider= MSDAORA;Data Source=10.1.4.93;User ID=grupo01;Password=servicios123;Unicode=True");
+             adaptador = new OleDbConnection("Provider= MSDAORA;Data Source=10.1.4.93;User ID=servicios_reservados;Password=servicios;Unicode=True");
              
 
        
