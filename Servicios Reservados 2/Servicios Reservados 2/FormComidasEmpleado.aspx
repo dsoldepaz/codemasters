14a29655b0ce918e72b1cf8adff68a24c58e1958
Daniel Picado <dprm23@gmail.com>
2015-05-13 08:49:54 -0600

    Wuju!! multi calendario!

---
+++ Servicios Reservados 2/Servicios Reservados 2/FormComidasEmpleado.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FormComidasEmpleado.aspx.cs" Inherits="Servicios_Reservados_2.FormComidasEmpleado" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <link rel="stylesheet" href="Content/ComidaEmpleado.css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Comidas de Empleado</h1>
+    <fieldset>
+         <h4>Seleccione el horario de comida</h4>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+        <div>
+            <input type="checkbox" value="Desayuno" />Desayuno
+            <input type="checkbox" value="Almuerzo" />Almuerzo
+            <input type="checkbox" value="Cena" />Cena
+            <input type="checkbox" value="Otro" />Otro
+        </div>
+        Fecha:<input runat="server" id="textFecha" onselect="fechaDeEntradaCalendario_SelectionChanged" disabled />
+                    <input class="selectorDeFecha" id="fechaDeEntrada" type="button" runat="server" onserverclick="fechaDeEntrada_ServerClick" />
+        
+                    <asp:Calendar ID="fechaDeEntradaCalendario" runat="server" OnSelectionChanged="fechaDeEntradaCalendario_SelectionChanged" OnDayRender="fechaDeEntradaCalendario_DayRender" BackColor="#CCCCCC" BorderStyle="Dashed" ForeColor="#7BC143" Height="80px" Width="100px" BorderWidth="1px" Visible="false">
+                        <DayHeaderStyle ForeColor="#333333" Wrap="True" />
+                        <DayStyle ForeColor="Black" />
+                        <NextPrevStyle ForeColor="Black" />
+                        <TitleStyle BackColor="#7BC143" ForeColor="#333333" />
+                    </asp:Calendar>
+
+            </ContentTemplate>
+   
+        </asp:UpdatePanel>
+
+    </fieldset>
+</asp:Content>
---
+++ Servicios Reservados 2/Servicios Reservados 2/FormComidasEmpleado.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Servicios_Reservados_2 {
+    
+    
+    public partial class FormComidasEmpleado {
+        
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Control textFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText textFecha;
+        
+        /// <summary>
+        /// Control fechaDeEntrada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton fechaDeEntrada;
+        
+        /// <summary>
+        /// Control fechaDeEntradaCalendario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar fechaDeEntradaCalendario;
+    }
+}
