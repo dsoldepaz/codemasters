d34103be8fd5a7d5cc5bcef23febb69b3006c38a
Daniel Picado <dprm23@gmail.com>
2015-05-20 10:37:30 -0600

    controladoraEmpleadoReserva

---
+++ Servicios Reservados 2/Servicios Reservados 2/App_Data/EmpleadoReserva/ControladoraEmpleadoReserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_Reservados_2
+{
+    public class ControladoraEmpleadoReserva
+    {
+        private ControladoraBDEmpleado controladoraEmpleado;
+        private ControladoraComidaEmpleado controladoraComidaEmpleado;
+        private ControladoraComidaCampo controladoraComidaCampo;
+
+    }
+}
