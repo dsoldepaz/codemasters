ea9b6e5e65aac3d9b99d1ac86814529d5cbaba97
dalivasquez7 <dali.vasquez7@gmail.com>
2015-06-17 09:55:38 -0600

    dataTable reportes

--- Servicios Reservados 2/Servicios Reservados 2/FormReportes.aspx.cs
+++ Servicios Reservados 2/Servicios Reservados 2/FormReportes.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -28,6 +29,82 @@ namespace Servicios_Reservados_2
             }
 		}
 
+        protected DataTable crearTablaReportes()//consultar
+        {
+            DataTable tabla = new DataTable();
+            DataColumn columna;
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Dìa";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Desayuno";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Desayunos Servidos";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Almuerzo";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Almuerzos Servidos";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Cena";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Cenas Servidos";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Snack";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Snack Servidos";
+            tabla.Columns.Add(columna);
+
+            columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Comida Campo";
+            tabla.Columns.Add(columna);
+
+              columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Comida Campo Servidos";
+            tabla.Columns.Add(columna);
+
+                  columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Total";
+            tabla.Columns.Add(columna);
+
+                  columna = new DataColumn();
+            columna.DataType = System.Type.GetType("System.String");
+            columna.ColumnName = "Total Servidos";
+            tabla.Columns.Add(columna);
+
+            GridViewReportes.DataSource = tabla;
+            GridViewReportes.AllowSorting = false;
+            GridViewReportes.DataBind();
+
+            return tabla;
+        }
 
 
 	}
