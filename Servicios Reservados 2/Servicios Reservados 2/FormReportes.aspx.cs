f3f4b1d119a2d3f571210f6822ae2189b97512c9
armandonl <armandonl14@gmail.com>
2015-06-30 15:00:54 -0600

    Grid reportes

--- Servicios Reservados 2/Servicios Reservados 2/FormReportes.aspx.cs
+++ Servicios Reservados 2/Servicios Reservados 2/FormReportes.aspx.cs
@@ -135,15 +135,16 @@ namespace Servicios_Reservados_2
          * Requiere: NA
          * Modifica: la tabla servicios, si la reservacion tiene servicios asociados
          * */
-        void llenarGridReportes()
+        void llenarGridReportes(String fecha)
         {
             DataTable tabla = crearTablaServicios();
             try
             {
 
+              
                 Object[] datos = new Object[13];
-
-                datos[0] = fechaInicial;
+    
+                datos[0] = fecha;
                 datos[1] = sumaTotalDesayuno;
                 datos[2] = sumaTotalConsumidosDesayuno;
                 datos[3] = sumaTotalAlmuerzo;
@@ -306,7 +307,7 @@ namespace Servicios_Reservados_2
                 while (inicio <= fin)//se realiza el for en el rango de fechas especificado
                 {
                     filtroEstacionAnfitrionaFecha(inicio.ToString("MM/dd/yyyy"), inicio.ToString("MM/dd/yyyy"));
-                    llenarGridReportes();
+                    llenarGridReportes(inicio.ToString("MM/dd/yyyy"));
                     inicio =  DateTime.Parse(inicio.AddDays(1).ToString("MM/dd/yyyy"));
                 }
             }
@@ -326,7 +327,7 @@ namespace Servicios_Reservados_2
             DataTable comidaCampoReservaPinto = verificarDataTable(controladora.obtenerComidaPax(estacion, 5, anfitriona, fechaInicio, fechaFinal)); //gallo pinto de reservacion
             DataTable comidaExtraSnack = verificarDataTable(controladora.obtenerComidaExtraEstacionAnfitrionaFecha(estacion, "Cena", anfitriona, fechaInicio, fechaFinal, 2));
 
-            if (anfitriona == 1)
+            if (anfitriona == 2)
             {
 
                 DataTable comidaCampoDesayunoEmp = verificarDataTable(controladora.obtenerComidaPaxEmp(estacion, 1, fechaInicio, fechaFinal)); //desayuno comida campo empleado
