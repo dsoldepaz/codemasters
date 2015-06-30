d29c36ccc399b9fd9fe7bc4aabdc59ff7a983879
dalivasquez7 <dali.vasquez7@gmail.com>
2015-06-30 14:10:34 -0600

    Todas las consultas de reportes

--- Servicios Reservados 2/Servicios Reservados 2/App_Data/Reportes/ControladoraBDReportes.cs
+++ Servicios Reservados 2/Servicios Reservados 2/App_Data/Reportes/ControladoraBDReportes.cs
@@ -24,11 +24,11 @@ namespace Servicios_Reservados_2
         }
 
         /* 
-         * Efecto: 
+         * Efecto: crea consulta para una comida campo de una reservacion utilizando todos los filtros.
          * Requiere: 
          * Modifica : 
          */
-        internal DataTable obtenerComidaPax(String estacion, int opcion, int anfitriona, String fecha, String fechaFinal)
+        internal DataTable obtenerComidaPax(String estacion, int opcion, int anfitriona, String fecha, String fechaFinal) //comida campo con todo reservacin
         {
             DataTable comidaCampo;
 
@@ -38,17 +38,44 @@ namespace Servicios_Reservados_2
         }
 
         /* 
-         * Efecto: 
-         * Requiere: 
-         * Modifica : 
-         */
-        internal DataTable obtenerComidaPaxEmp(String estacion, int opcion, String fecha, String fechaFinal)
+      * Efecto: crea consulta para una comida campo de una reservacion utilizando el filtro de estacion y fechas.
+      * Requiere: 
+      * Modifica : 
+      */
+        internal DataTable obtenerComidaPaxEstacionFecha(String estacion, int opcion, String fecha, String fechaFinal) //comida campo con todo reservacin
         {
-            DataTable comidaCampoEmp;
+            DataTable comidaCampo;
 
-            String consultaSQL = "select distinct fecha, sum(pax), sum(vecesconsumido) from comida_campo where estacion = '" + estacion + "' and opcion = " + opcion + " and to_date(fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') and idempleado is not null group by fecha";
-            comidaCampoEmp = adaptador.consultar(consultaSQL);
-            return comidaCampoEmp;
+            String consultaSQL = "select distinct campo.fecha, SUM(campo.pax), SUM(campo.vecesconsumido) from (reservas.vr_reservacion vista JOIN reservas.reservacion reserva ON vista.numero = reserva.numero) JOIN comida_campo campo ON reserva.id = campo.idreservacion WHERE opcion = " + opcion + " and vista.estacion = '" + estacion + "' and to_date(campo.fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(campo.fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') group by campo.fecha";
+            comidaCampo = adaptador.consultar(consultaSQL);
+            return comidaCampo;
+        }
+
+        /* 
+      * Efecto: crea consulta para una comida campo de una reservacion utilizando filtro de anfitriona y fechas.
+      * Requiere: 
+      * Modifica : 
+      */
+        internal DataTable obtenerComidaPaxAnfitrionaFecha(int opcion, int anfitriona, String fecha, String fechaFinal) //comida campo con todo reservacin
+        {
+            DataTable comidaCampo;
+
+            String consultaSQL = "select distinct campo.fecha, SUM(campo.pax), SUM(campo.vecesconsumido) from (reservas.vr_reservacion vista JOIN reservas.reservacion reserva ON vista.numero = reserva.numero) JOIN comida_campo campo ON reserva.id = campo.idreservacion WHERE opcion =  " + opcion + " and vista.idanfitriona = " + anfitriona + "  and to_date(campo.fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(campo.fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') group by campo.fecha";
+            comidaCampo = adaptador.consultar(consultaSQL);
+            return comidaCampo;
+        }
+
+        /* 
+   * Efecto: crea consulta para una comida campo de una reservacion utilizando filtro solo de fechas.
+   * Requiere: 
+   * Modifica : 
+   */
+        internal DataTable obtenerComidaPaxFechas(int opcion, String fecha, String fechaFinal){ //comida campo reservacion con solo fechas.
+            DataTable comidaCampo;
+
+            String consultaSQL = "select distinct campo.fecha, sum(campo.pax), sum(vecesconsumido) from (reservas.vr_reservacion vista JOIN reservas.reservacion reserva ON vista.numero = reserva.numero) JOIN comida_campo campo ON reserva.id = campo.idreservacion WHERE opcion = " + opcion + " and to_date(campo.fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(campo.fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') group by campo.fecha";
+            comidaCampo = adaptador.consultar(consultaSQL);
+            return comidaCampo;
         }
 
         /* 
@@ -56,7 +83,30 @@ namespace Servicios_Reservados_2
          * Requiere: 
          * Modifica : 
          */
-        internal DataTable obtenerComidaEmp(String estacion, String opcion, String fecha, String fechaFinal)
+        internal DataTable obtenerComidaPaxEmp(String estacion, int opcion, String fecha, String fechaFinal) //comida campo con todo empleado o solo con estacion y fecha porque anfitriones siempre ESINTRO
+        {
+            DataTable comidaCampoEmp;
+
+            String consultaSQL = "select distinct fecha, sum(pax), sum(vecesconsumido) from comida_campo where estacion = '" + estacion + "' and opcion = " + opcion + " and to_date(fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') and idempleado is not null group by fecha";
+            comidaCampoEmp = adaptador.consultar(consultaSQL);
+            return comidaCampoEmp;
+        }
+       
+        internal DataTable obtenerComidaPaxEmpFechas(int opcion, String fecha, String fechaFinal) //comida campo empleado con solo fechas o con anfitriona, pero siempre es ESINTRO.
+        {
+            DataTable comidaCampoEmp;
+
+            String consultaSQL = "select distinct fecha, sum(pax), sum(vecesconsumido) from comida_campo where opcion = " + opcion + " and to_date(fecha, 'MM/dd/yyyy') <= to_date('" + fechaFinal + "', 'MM/dd/yyyy') and to_date(fecha, 'MM/dd/yyyy') >= to_date('" + fecha + "', 'MM/dd/yyyy') and idempleado is not null group by fecha";
+            comidaCampoEmp = adaptador.consultar(consultaSQL);
+            return comidaCampoEmp;
+        }
+
+        /* 
+         * Efecto: 
+         * Requiere: 
+         * Modifica: 
+         */
+        internal DataTable obtenerComidaEmp(String estacion, String opcion, String fecha, String fechaFinal) //comida campo empleado con todo o con solo estacion y fechas.
         {
             DataTable comidaCampoEmp;
 
@@ -64,7 +114,32 @@ namespace Servicios_Reservados_2
             comidaCampoEmp = adaptador.consultar(consultaSQL);
             return comidaCampoEmp;
         }
+        /* 
+      * Efecto: 
+      * Requiere: 
+      * Modifica: 
+      */
+        internal DataTable obtenerComidaEmpEstacionFechas(String opcion, String fecha, String fechaFinal) //comida campo empleado con todo. o solo con anfitriona y fechas porque anfitriona = ESINTRO.
+        {
+            DataTable comidaCampoEmp;
 
+            String consultaSQL = "select distinct fecha, count(" + opcion + "), count(vecesconsumido) from reserva_empleado where " + opcion + " = 'R' and fecha >= to_date('" + fecha + "', 'mm/dd/yyyy') and fecha <= to_date('" + fechaFinal + "', 'mm/dd/yyyy') group by fecha";
+            comidaCampoEmp = adaptador.consultar(consultaSQL);
+            return comidaCampoEmp;
+        }
+        /* 
+      * Efecto: 
+      * Requiere: 
+      * Modifica: 
+      */
+        internal DataTable obtenerComidaEmp(String estacion, String opcion, String fecha, String fechaFinal) //comida campo empleado con todo.
+        {
+            DataTable comidaCampoEmp;
+
+            String consultaSQL = "select distinct fecha, count(" + opcion + "), count(vecesconsumido) from reserva_empleado where " + opcion + " = 'R' and fecha >= to_date('" + fecha + "', 'mm/dd/yyyy') and fecha <= to_date('" + fechaFinal + "', 'mm/dd/yyyy') and estacion ='" + estacion + "'group by fecha";
+            comidaCampoEmp = adaptador.consultar(consultaSQL);
+            return comidaCampoEmp;
+        }
         /* 
          * Efecto: crea la consulta que filtra comida extra por un rango de fecha.
          * Requiere: la entrada de las variables que realizan el filtrado.
