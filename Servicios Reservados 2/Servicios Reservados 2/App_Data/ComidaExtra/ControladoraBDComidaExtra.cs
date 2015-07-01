5842b2fd584574912ad3f2121adc08efa59c4897
dsoldepaz <d.soldepaz@gmail.com>
2015-07-01 14:33:09 -0600

    mensaje de error anular comida extra

--- Servicios Reservados 2/Servicios Reservados 2/App_Data/ComidaExtra/ControladoraBDComidaExtra.cs
+++ Servicios Reservados 2/Servicios Reservados 2/App_Data/ComidaExtra/ControladoraBDComidaExtra.cs
@@ -116,7 +116,7 @@ namespace Servicios_Reservados_2
 
                 respuesta[0] = "success";
                 respuesta[1] = "Exito. ";
-                respuesta[2] = "La comida extra se ha eliminado exitosamente";
+                respuesta[2] = "La comida extra se ha anulado exitosamente";
             }
             catch (SqlException e)
             {
